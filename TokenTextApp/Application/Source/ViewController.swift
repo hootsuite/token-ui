ca5dba00b4316a73c08264deb29e1fbfe7e0d6ec
David Bonnefoy <david.bonnefoy@hootsuite.com>
2016-05-27 10:16:58 -0700

    PUB-4000: Demo app setup.

---
+++ TokenTextApp/Application/Source/ViewController.swift
@@ -0,0 +1,14 @@
+//
+//  ViewController.swift
+//  TokenTextApp
+//
+//  Created by David Bonnefoy on 2016-05-27.
+//  Copyright © 2016 Hootsuite. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ViewController: UIViewController {
+
+}

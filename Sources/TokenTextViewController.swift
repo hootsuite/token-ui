3f808571839d828cf04e46d65d31217d2610671b
steve-sun-hs <Steve.Sun@hootsuite.com>
2017-10-24 08:59:15 -0700

    PUB-9385: Textview input in Compose is Odd (#24)
    * PUB-9385: Textview input in Compose is Odd
    
    * PUB-9385: Addressed commented issue

--- Sources/TokenTextViewController.swift
+++ Sources/TokenTextViewController.swift
@@ -612,10 +612,9 @@ open class TokenTextViewController: UIViewController, UITextViewDelegate, NSLayo
     fileprivate func normalModeTap(recognizer: UITapGestureRecognizer) {
         viewAsTextView.becomeFirstResponder()
         let location: CGPoint = recognizer.location(in: viewAsTextView)
-        var charIndex = viewAsTextView.characterIndexAtLocation(location)
-        if charIndex != nil && charIndex! < viewAsTextView.textStorage.length - 1 {
+        if let charIndex = viewAsTextView.characterIndexAtLocation(location), charIndex < viewAsTextView.textStorage.length - 1 {
             var range = NSRange(location: 0, length: 0)
-            if let tokenRef = viewAsTextView.attributedText?.attribute(TokenTextViewControllerConstants.tokenAttributeName, at: charIndex!, effectiveRange: &range) as? TokenReference {
+            if let tokenRef = viewAsTextView.attributedText?.attribute(TokenTextViewControllerConstants.tokenAttributeName, at: charIndex, effectiveRange: &range) as? TokenReference {
                 _ = resignFirstResponder()
                 let rect: CGRect = {
                     if let textRange = viewAsTextView.textRangeFromNSRange(range) {
@@ -625,12 +624,6 @@ open class TokenTextViewController: UIViewController, UITextViewDelegate, NSLayo
                     }
                 }()
                 delegate?.tokenTextViewDidSelectToken(self, tokenRef: tokenRef, fromRect: rect)
-            } else {
-                if charIndex == viewAsTextView.textStorage.length - 1 {
-                    // Allow placing the cursor at the end of the text
-                    charIndex = viewAsTextView.textStorage.length
-                }
-                viewAsTextView.selectedRange = NSRange(location: charIndex!, length: 0)
             }
         }
     }

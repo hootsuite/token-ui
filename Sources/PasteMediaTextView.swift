0488f3b038af14fdae2f8e69338942a859968e4b
Tobias Carryer <tobias.carryer@hootsuite.com>
2018-07-11 16:16:53 -0700

    CMOB-0: Expose struct init for unit tests

--- Sources/PasteMediaTextView.swift
+++ Sources/PasteMediaTextView.swift
@@ -25,10 +25,12 @@ public enum PasteboardItemType: String {
 
 /// A data structure used to hold pasteboard data and it's `PasteboardItemType`.
 public struct PasteboardItem {
-
+    public init( type: PasteboardItemType, data: Data ) {
+        self.type = type
+        self.data = data
+    }
     public let type: PasteboardItemType
     public let data: Data
-
 }
 
 /// A custom `UITextView` subclass used to accept specific `PasteboardItemType`'s.

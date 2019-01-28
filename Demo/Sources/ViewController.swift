f803fda85e201afab62555cd7c20aba659bf17bf
Albert Castro <albert.castro@hootsuite.com>
2019-01-28 10:59:35 -0800

    text change

--- Demo/Sources/ViewController.swift
+++ Demo/Sources/ViewController.swift
@@ -22,7 +22,7 @@ class ViewController: UIViewController {
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         tokenTextVC.font = UIFont(name: "HelveticaNeue", size: 19)!
-        tokenTextVC.text = "Hello "
+        tokenTextVC.text = "Hola! "
         tokenTextVC.addToken(6, text: "Team")
     }
 

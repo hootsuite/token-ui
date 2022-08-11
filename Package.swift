8dbad378a86544a26557ab6fbc757fa20b28e17d
Prashant Bhargava <62952881+prashant-bhargava-hs@users.noreply.github.com>
2022-08-11 10:24:25 -0700

    MBL-5781: Add SPM support (#40)
    * add package.swift
    
    * correct test target
    
    * update swift tools version
    
    * Update Package.swift

---
+++ Package.swift
@@ -0,0 +1,16 @@
+// swift-tools-version:5.3
+
+import PackageDescription
+
+let package = Package(
+    name: "TokenUI",
+    products: [
+        .library(name: "TokenUI", targets: ["TokenUI"]),
+    ],
+    dependencies: [
+    ],
+    targets: [
+        .target(name: "TokenUI", path: "Sources"),
+        .testTarget(name: "TokenTests", dependencies: ["TokenUI"], path: "Tests")
+    ]
+)

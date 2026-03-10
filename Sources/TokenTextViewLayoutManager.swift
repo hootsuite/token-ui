58c9e8a13b73dcd6fad96c5844d0a904ea05976f
martin.tsang@hootsuite.com <martin.tsang@hootsuite.com>
2026-03-10 08:34:26 -0700

    Square edges

--- Sources/TokenTextViewLayoutManager.swift
+++ Sources/TokenTextViewLayoutManager.swift
@@ -6,14 +6,12 @@ import UIKit
 class TokenTextViewLayoutManager: NSLayoutManager {
 
     override func fillBackgroundRectArray(_ rectArray: UnsafePointer<CGRect>, count rectCount: Int, forCharacterRange charRange: NSRange, color: UIColor) {
-        // FIXME: check attributes
-        for i in 0..<rectCount {
-            let backgroundRect = rectArray[i].insetBy(dx: -6, dy: 1)
-            let path = UIBezierPath(roundedRect: backgroundRect, cornerRadius: 20)
-            path.fill()
-            path.stroke()
-        }
         color.set()
+        for i in 0..<rectCount {
+            let backgroundRect = rectArray[i]
+            let path = UIBezierPath(rect: backgroundRect)
+            path.fill()
+        }
     }
 
 }
